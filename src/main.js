5eb6ade96d252a0333034b31edeffb79dd509887
Al14lex <alenahomenko1421@gmail.com>
2024-04-01 22:49:20 +0200

    455

--- src/main.js
+++ src/main.js
@@ -4,10 +4,8 @@ import 'izitoast/dist/css/iziToast.min.css';
 import { fetchImages } from "./js/pixabay-api";
 import {renderImg} from "./js/render-functions"
 
-let page = 1;
-let query;
-let maxPage = 0;
 const perPage = 15;
+let page = 1, query, maxPage = 0;
 
 export const select = {
   form: document.querySelector('.form'),
@@ -20,45 +18,31 @@ export const select = {
 select.form.addEventListener('submit', submitBtn);
 select.btnMore.addEventListener('click', clickloadMore);
 
-function showLoadMore() {
-  select.btnMore.classList.remove('is-hidden');
-}
-function hideLoadMore() {
-  select.btnMore.classList.add('is-hidden');
-}
-function deleteLoader() {
-  select.loader.classList.add('is-hidden');
-}
-function addLoader() {
-  select.loader.classList.remove('is-hidden');
-}
+const toggleLoader = (show) => select.loader.classList[show ? 'remove' : 'add']('is-hidden');
+const toggleLoadMore = (show) => select.btnMore.classList[show ? 'remove' : 'add']('is-hidden');
 
-deleteLoader();
-hideLoadMore();
+toggleLoader(false);
+toggleLoadMore(false);
 
 async function submitBtn(event) {
   event.preventDefault();
-  addLoader();
-
+  toggleLoader(true);
   query = select.input.value.trim();
   page = 1;
   select.gallery.innerHTML = ''
-
   if (!query) {
     iziToast.error({
       message: 'Please enter a request',
       position: 'topRight',
     });
-
-    hideLoadMore();
+    toggleLoadMore(false);
     scroll();
     return;
-  }
-
-  try {
+  } try {
     const data = await fetchImages(query, page);
     if (data.hits.length === 0) {
-       deleteLoader();
+      toggleLoadMore(false);
+      toggleLoader(false);
       iziToast.error({
         message:
           'Sorry, there are no images matching your search query. Please try again!',
@@ -68,48 +52,38 @@ async function submitBtn(event) {
     }
     maxPage = Math.ceil(data.totalHits / perPage);
     renderImg(data.hits);
-  } catch (error) {
-    console.log(error);
-  }
-
-  deleteLoader();
+  } catch (error) {console.error(error);}
+  toggleLoader(false);
   finishItems();
   select.form.reset();
 }
 
 async function clickloadMore() {
   page += 1;
-  addLoader();
-
+  toggleLoader(true);
   try {
     const data = await fetchImages(query, page);
     renderImg(data.hits);
-  } catch (error) {
-    console.log(error);
-  }
-
+  } catch (error) {console.error(error);}
   scroll();
   finishItems();
-  deleteLoader();
+  toggleLoader(false);
 }
 
 function finishItems() {
   if (page >= maxPage) {
-    hideLoadMore();
+    toggleLoadMore(false);
     iziToast.error({
       message: "We're sorry, but you've reached the end of search results.",
       position: 'topRight',
       color: 'yellow'
     });
-  } else {
-    showLoadMore();
-  }
+  } else {toggleLoadMore(true);}
 }
 
 function scroll() {
   const cardHeight = select.gallery.firstChild.getBoundingClientRect().height;
   const currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-  
   window.scroll({
     top: currentScrollPosition + cardHeight * 2,
     behavior: 'smooth',
